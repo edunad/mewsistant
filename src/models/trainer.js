da6ff9a7a80f99d6c70bbaa423ac28a5ad863ae5
D3lta <D3lta@users.noreply.github.com>
2019-07-03 21:27:21 +0200

    added .DS_STORE to ignore

--- src/models/trainer.js
+++ src/models/trainer.js
@@ -2,8 +2,8 @@ const cv = require('opencv4nodejs');
 const tf = require('@tensorflow/tfjs-node');
 const fs = require('fs-extra');
 
-const epoch = 500;
-const batches = 100;
+const epoch = 400;
+const batches = 30;
 
 module.exports = class Trainer {
     constructor(trainingData, size) {
@@ -19,7 +19,7 @@ module.exports = class Trainer {
 
     initialize(onDone) {
         cv.imreadAsync('./train_data/mask.png', (err, mask) => {
-            if(err) throw new Error('Failed to load line mask');
+            if (err) throw new Error('Failed to load line mask');
 
             this.lineMask = mask;
             return onDone();
@@ -33,8 +33,8 @@ module.exports = class Trainer {
     }
 
     saveModel(onSave) {
-        if(this.AImodel == null) return onSave('Meow brain not initialized');
-        
+        if (this.AImodel == null) return onSave('Meow brain not initialized');
+
         fs.ensureFile('./brains/meow_tail.json').then(() => {
             this.AImodel.save('file://./brains/meow_brain').then(() => {
                 fs.writeJSON('./brains/meow_tail.json', this.letterDatabase).then(() => {
@@ -47,7 +47,7 @@ module.exports = class Trainer {
     }
 
     loadModel(onLoad) {
-        if(this.AImodel != null) return onLoad('Meow brain already initialized');
+        if (this.AImodel != null) return onLoad('Meow brain already initialized');
         fs.readJSON('./brains/meow_tail.json').then((jsonData) => {
             this.letterDatabase = jsonData;
 
@@ -61,46 +61,46 @@ module.exports = class Trainer {
     }
 
     predict(slide, onDone) {
-            const img = cv.imdecode(slide);
-            // if(err) throw new Error(err);
+        const img = cv.imdecode(slide);
+        // if(err) throw new Error(err);
 
-            let regions = this.splitImage(img, this.lineMask);
-            if(regions == null) return onDone(null);
+        let regions = this.splitImage(img, this.lineMask);
+        if (regions == null) return onDone(null);
 
-            let result = [];
+        let result = [];
 
-            let total = regions.length;
-            let done = () => {
-                total--;
-                if(total <= 0) return onDone(result);
-            };
+        let total = regions.length;
+        let done = () => {
+            total--;
+            if (total <= 0) return onDone(result);
+        };
 
-            for(let i = 0; i < regions.length; i++) {
-                let region = regions[i];
+        for (let i = 0; i < regions.length; i++) {
+            let region = regions[i];
 
-                let mat = img.getRegion(new cv.Rect(region[0], region[1], region[2], region[3]));
-                mat = mat.resize(new cv.Size(this.size, this.size)); // Scale all to 28x28
+            let mat = img.getRegion(new cv.Rect(region[0], region[1], region[2], region[3]));
+            mat = mat.resize(new cv.Size(this.size, this.size)); // Scale all to 28x28
 
-                let x = new tf.tensor3d([this.convertRGBData(mat)]);
-                let prediction = this.AImodel.predict(x);
+            let x = new tf.tensor3d([this.convertRGBData(mat)]);
+            let prediction = this.AImodel.predict(x);
 
-                prediction.data().then((lePrediction) => {
-                    let magic = Array.from(lePrediction).map((p, i) => {
-                        return {
-                            probability: p,
-                            value: i
-                        };
-                    }).sort(function(a,b){
-                        return b.probability - a.probability;
-                    }).slice(0, 2);
-                    
-                    let chosenOne = magic[0];
-                    result.push(this.letterDatabase[chosenOne.value]);
+            prediction.data().then((lePrediction) => {
+                let magic = Array.from(lePrediction).map((p, i) => {
+                    return {
+                        probability: p,
+                        value: i
+                    };
+                }).sort(function (a, b) {
+                    return b.probability - a.probability;
+                }).slice(0, 2);
 
-                    return done();
-                });
-            }
-        
+                let chosenOne = magic[0];
+                result.push(this.letterDatabase[chosenOne.value]);
+
+                return done();
+            });
+        }
+
     }
 
     buildTrainModel(done) {
@@ -110,11 +110,11 @@ module.exports = class Trainer {
         // Keras or whatever this is
         this.AImodel = tf.sequential();
 
-        this.AImodel.add(tf.layers.flatten({inputShape: [this.size, this.size]})); // Size of the training samples
-        this.AImodel.add(tf.layers.dense({units: 128, activation: 'relu'})); // 128 layers
-        this.AImodel.add(tf.layers.dense({units: Object.keys(this.weightData).length, activation: 'softmax'})); // Not entirely sure
+        this.AImodel.add(tf.layers.flatten({ inputShape: [this.size, this.size] })); // Size of the training samples
+        this.AImodel.add(tf.layers.dense({ units: 128, activation: 'relu' })); // 128 layers
+        this.AImodel.add(tf.layers.dense({ units: Object.keys(this.weightData).length, activation: 'softmax' })); // Not entirely sure
 
-        this.AImodel.compile({optimizer: 'adam', loss: 'sparseCategoricalCrossentropy', metrics: 'accuracy'});
+        this.AImodel.compile({ optimizer: 'adam', loss: 'sparseCategoricalCrossentropy', metrics: 'accuracy' });
 
         // All pixel data to, equivelant to the letter weight
         // [[123,123123,123],[234,2342,3423,4],[234,23,4,2,34,2],[12,234,234,2]] => [1 5 7 9]
@@ -125,7 +125,7 @@ module.exports = class Trainer {
         Object.keys(this.trainingData).forEach((slide) => {
             let answer = this.trainingData[slide];
             let w = [];
-            for(let i = 0; i < answer.length; i++) {
+            for (let i = 0; i < answer.length; i++) {
                 let char = answer[i];
 
                 trainData.push(this.letterData[char + '_' + slide]);
@@ -136,7 +136,7 @@ module.exports = class Trainer {
         let x = new tf.tensor3d(trainData);
         let y = new tf.tensor1d(weights);
 
-        this.AImodel.fit(x, y, {epochs: epoch, batchSize: batches, verbose: 1}).then(() => {
+        this.AImodel.fit(x, y, { epochs: epoch, batchSize: batches, verbose: 1 }).then(() => {
             tf.tidy(() => {
                 return done();
             });
@@ -145,34 +145,34 @@ module.exports = class Trainer {
         });
     }
 
-    rgbToInt(r, g, b){
+    rgbToInt(r, g, b) {
         // Convert it to a 0 -> 1 value
-        return (r * 299/1000 + g * 587/1000 + b * 114/1000) / 255;
+        return (r * 299 / 1000 + g * 587 / 1000 + b * 114 / 1000) / 255;
     }
 
     prepareData(callback) {
         let total = Object.keys(this.trainingData).length;
         let onDone = () => {
             total--;
-            if(total <= 0) return callback();
+            if (total <= 0) return callback();
         };
-        
+
         Object.keys(this.trainingData).forEach((slide, indx) => {
             cv.imreadAsync(`./train_data/img/${slide}`, (err, img) => {
-                if(err) throw new Error(err);
+                if (err) throw new Error(err);
 
                 let regions = this.splitImage(img, this.lineMask);
                 if (regions == null) throw new Error(`Invalid training data ${slide}, could not split :(`);
 
-                for(let i = 0; i < regions.length; i++) {
+                for (let i = 0; i < regions.length; i++) {
                     let letter = this.trainingData[slide][i];
                     let region = regions[i];
-        
+
                     let mat = img.getRegion(new cv.Rect(region[0], region[1], region[2], region[3]));
                     mat = mat.resize(new cv.Size(this.size, this.size)); // Scale all to 28x28
-        
+
                     this.letterData[letter + '_' + slide] = this.convertRGBData(mat);
-                    if(this.letterDatabase.indexOf(letter) == -1) this.letterDatabase.push(letter);
+                    if (this.letterDatabase.indexOf(letter) == -1) this.letterDatabase.push(letter);
                 }
 
                 return onDone();
@@ -181,30 +181,30 @@ module.exports = class Trainer {
     }
 
     splitImage(slide, mask) {
-        if(mask == null) throw new Error('Invalid line mask');
+        if (mask == null) throw new Error('Invalid line mask');
 
         let grayMask = mask.cvtColor(cv.COLOR_BGR2GRAY);
         let gray = slide.cvtColor(cv.COLOR_BGR2GRAY);
-        let smooth = gray.gaussianBlur(new cv.Size(3, 3), 0, 0 );
+        let smooth = gray.gaussianBlur(new cv.Size(3, 3), 0, 0);
 
         let dilation_size = 4;
-        let dilateStructure = cv.getStructuringElement(cv.MORPH_RECT, new cv.Size(2*dilation_size + 1, 2*dilation_size+1), new cv.Point( dilation_size, dilation_size ));
+        let dilateStructure = cv.getStructuringElement(cv.MORPH_RECT, new cv.Size(2 * dilation_size + 1, 2 * dilation_size + 1), new cv.Point(dilation_size, dilation_size));
         smooth = smooth.erode(dilateStructure);
 
         let slideThresh = smooth.adaptiveThreshold(255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 185, 18);
 
         let borderSize = 5;
-        grayMask.drawRectangle(new cv.Point2(borderSize / 2, borderSize / 2), new cv.Point2(slide.sizes[1] - borderSize / 2, slide.sizes[0] - borderSize / 2), 
-                                new cv.Vec3(255, 255, 255), borderSize);
+        grayMask.drawRectangle(new cv.Point2(borderSize / 2, borderSize / 2), new cv.Point2(slide.sizes[1] - borderSize / 2, slide.sizes[0] - borderSize / 2),
+            new cv.Vec3(255, 255, 255), borderSize);
 
         grayMask = grayMask.add(slideThresh);
 
         dilation_size = 6;
-        dilateStructure = cv.getStructuringElement(cv.MORPH_RECT, new cv.Size(2, 2*dilation_size+1), new cv.Point( 0.5, dilation_size ));
+        dilateStructure = cv.getStructuringElement(cv.MORPH_RECT, new cv.Size(2, 2 * dilation_size + 1), new cv.Point(0.5, dilation_size));
         grayMask = grayMask.erode(dilateStructure);
 
         let finalTresh = grayMask.threshold(1, 30, cv.THRESH_BINARY_INV | cv.THRESH_OTSU);
-        
+
         let contours = finalTresh.findContours(cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE);
         let regions = [];
 
@@ -217,15 +217,15 @@ module.exports = class Trainer {
         });
 
         // sort hulls using X
-        regions.sort((a, b) => { 
-            if(a[0] > b[0]) return 1;
+        regions.sort((a, b) => {
+            if (a[0] > b[0]) return 1;
             return -1;
         });
 
         if (regions.length != 6) return null;
         return regions;
     }
-    
+
     convertRGBData(mat) {
         let matData = mat.getDataAsArray();
         let fixedData = [];
@@ -235,7 +235,7 @@ module.exports = class Trainer {
             colorRow.forEach((colorCol) => {
                 newRow.push(this.rgbToInt(colorCol[2], colorCol[1], colorCol[0])); // BGR
             });
-            
+
             fixedData.push(newRow);
         });
 
